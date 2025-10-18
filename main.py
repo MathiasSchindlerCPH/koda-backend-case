be54e62bc2b5254aa8692f5f091db838daf1b6ed
Mathias Schindler <mathingvid@gmail.com>
2025-10-18 14:16:54 +0200

    chore(main): reconfigure to call parametized classes appropriately

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ def main():
         input_path=os.path.join(data_dir, "MOCK_STREAMING_Q4_2023.json"),
         output_path=os.path.join(output_dir, "MOCK_STREAMING_Q4_2023_transformed.csv"),
     )
-    json_task.run()
+    df_json = json_task.run()
+    json_task.export_csv(df_json)
 
     # CSV konvertering
     csv_task = CsvConverter(
         input_path=os.path.join(data_dir, "RADIO_MOCK_FM.csv"),
         output_path=os.path.join(output_dir, "RADIO_MOCK_FM_converted.csv"),
     )
-    csv_task.run()
+    df_csv = csv_task.run()       
+    csv_task.export_csv(df_csv)   
 
 if __name__ == "__main__":
     main()
